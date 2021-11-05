7a9c12ecfbe09c0a0cbbcb72b452a853736efe70
johanbert <johanbertmosquera@gmail.com>
2021-11-04 22:09:18 -0500

    Promises solved frontend and backend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,4 +6,32 @@ const getApi = async() => {
     console.log('data', data)
     return data
 }
+
+const drawGet = async() => {
+    const response = await getApi()
+    console.log('response', response)
+
+    for (let i = 0; i < response.length; i++) {
+        // for (key in response) {
+        // console.log('key', key)
+        document.getElementById("estudiantesBrillantes").innerHTML += `
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://mdbootstrap.com/img/new/standard/city/043.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">${response[i].nombre} - ${response[i].edad}</h5>
+                    <p class="card-text">
+                        This card has supporting text below as a natural lead-in to additional content.
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">${response[i].fecha}</small>
+                </div>
+            </div>
+        </div>`
+    }
+}
+
+
 getApi()
+drawGet()
